3898ac89a34fd0bfde64c60b99b01699aa3273d2
Gabriel Schincariol <gschin11@gmail.com>
2023-05-14 14:35:28 -0300

    Adicionar nova tela de login e fetch

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,1 +1,11 @@
-console.log("javascript online");
+fetch('http://localhost:8080/teste')
+    .then(response => response.json())
+    .then(data => {
+        const lista = document.getElementById('lista');
+        data.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = `${item.id} - ${item.gabriel}`;
+            lista.appendChild(li);
+        });
+    })
+    .catch(error => console.error(error));
