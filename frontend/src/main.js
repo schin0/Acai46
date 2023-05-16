6c5c7ba60bef5ef563d0f24857dea2b502058a73
Gabriel Schincariol <gschin11@gmail.com>
2023-05-16 20:00:09 -0300

    Criar nova tela para home

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,15 @@
-// fetch('http://localhost:8080/teste')
-//     .then(response => response.json())
-//     .then(data => {
-//         const lista = document.getElementById('lista');
-//         data.forEach(item => {
-//             const li = document.createElement('li');
-//             li.textContent = `${item.id} - ${item.gabriel}`;
-//             lista.appendChild(li);
-//         });
-//     })
-//     .catch(error => console.error(error));
+fetch('http://localhost:8080/teste')
+    .then(response => response.json())
+    .then(data => {
+        const lista = document.getElementById('lista');
+        data.forEach(item => {
+            const li = document.createElement('li');
+            li.textContent = `${item.id} - ${item.gabriel}`;
+            lista.appendChild(li);
+        });
+    })
+    .catch(error => console.error(error));
+
+document.getElementById('btn-entrar').addEventListener('click', () => {
+    window.location.href = 'home/home.html';
+});
