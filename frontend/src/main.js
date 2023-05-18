9c066984104977635f3383415258479068ea6d94
Gabriel Schincariol <gschin11@gmail.com>
2023-05-17 21:14:22 -0300

    Inserir funcionalidade de login no front

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,15 +1,37 @@
-fetch('http://localhost:8080/teste')
-    .then(response => response.json())
-    .then(data => {
-        const lista = document.getElementById('lista');
-        data.forEach(item => {
-            const li = document.createElement('li');
-            li.textContent = `${item.id} - ${item.gabriel}`;
-            lista.appendChild(li);
-        });
-    })
-    .catch(error => console.error(error));
+function efetuarLogin(usuario, senha) {
+    const dados = {
+        usuario: usuario,
+        senha: senha
+    };
 
-document.getElementById('btn-entrar').addEventListener('click', () => {
-    window.location.href = 'home/home.html';
-});
+    // Faz a requisição POST para o endpoint de login
+    fetch('http://localhost:8080/colaboradores/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(dados)
+    })
+        .then(response => response.json())
+        .then(sucesso => {
+            if (sucesso) {
+                window.location.href = 'home/home.html';
+                return;
+            }
+
+            // Lógica para lidar com a resposta do servidor após o login
+            console.log("usuário e/ou senha incorreto(s)!");
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+function processarLogin() {
+    const usuario = document.getElementById('usuario').value;
+    const senha = document.getElementById('senha').value;
+
+    efetuarLogin(usuario, senha);
+}
+
+document.getElementById('btn-entrar').addEventListener('click', processarLogin);
