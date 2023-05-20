6902b6085cc11e432a40821f5b8e9723b274735e
Gabriel Schincariol <gschin11@gmail.com>
2023-05-20 20:08:51 -0300

    Criar métodos de adição e edição

--- backend/src/main/java/godigital/controller/ColaboradorController.java
+++ backend/src/main/java/godigital/controller/ColaboradorController.java
@@ -4,11 +4,15 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import godigital.dto.ColaboradorDTO;
+import godigital.model.Colaborador;
 import godigital.service.ColaboradorService;
 
 @RestController
@@ -32,6 +36,18 @@ public class ColaboradorController {
     @GetMapping("obter/")
     public ColaboradorDTO obterColaborador(@RequestParam(required = false) Long id) {
         return colaboradorService.obterColaborador(id);
+    }
+
+    @CrossOrigin(origins = "*")
+    @PutMapping("editar/")
+    public ColaboradorDTO editarColaborador(@RequestBody ColaboradorDTO colaboradorDto) {
+        return colaboradorService.editarColaborador(colaboradorDto);
+    }
+    
+    @CrossOrigin(origins = "*")
+    @PostMapping("adicionar/")
+    public ColaboradorDTO adicionarColaborador(@RequestBody Colaborador colaborador) {
+        return colaboradorService.adicionarColaborador(colaborador);
     }
 }
 
--- backend/src/main/java/godigital/service/ColaboradorService.java
+++ backend/src/main/java/godigital/service/ColaboradorService.java
@@ -40,5 +40,30 @@ public class ColaboradorService {
     	return new ColaboradorDTO(colaborador);
     }
     
+    public ColaboradorDTO editarColaborador(ColaboradorDTO colaboradorDto) {
+    	var colaboradorAtual = colaboradorRepository.findById(colaboradorDto.getId());
+    	var colaboradorRetorno = new ColaboradorDTO();
+    	
+    	if (colaboradorAtual.isPresent()) {
+    		var colaborador = colaboradorAtual.get();
+    		
+    		colaborador.setNome(colaboradorDto.getNome());
+    		colaborador.setEmail(colaboradorDto.getEmail());
+    		colaborador.setCpf(colaboradorDto.getCpf());
+    		colaborador.setCargo(colaboradorDto.getCargo());
+    		colaborador.setDataNascimento(colaboradorDto.getDataNascimento());
+    		colaborador.setDataAdmissao(colaboradorDto.getDataAdmissao());
+    		
+    		colaboradorRetorno = new ColaboradorDTO(colaboradorRepository.save(colaborador));
+    	}
+
+    	return colaboradorRetorno;
+    }
+    
+    public ColaboradorDTO adicionarColaborador(Colaborador colaborador) {
+    	colaborador.setId(1L);
+    	return new ColaboradorDTO(colaboradorRepository.save(colaborador));
+    }
+    
 }
 
