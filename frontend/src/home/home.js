fffe0ae7b1f7d7062da82fec8ceb2167be594b6a
Gabriel Schincariol <gschin11@gmail.com>
2023-05-20 00:38:04 -0300

    Adicionar tela para listagem de colaboradores

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -1,31 +1,45 @@
-console.log('oi');
+const pesquisa = document.getElementById('nome-colaborador');
 
-fetch('http://localhost:8080/colaboradores', {
-    method: 'GET'
-})
-    .then(response => response.json())
-    .then(retorno => {
-        console.log(retorno);
-        const section = document.getElementById('lista');
-        const lista = document.createElement('ul');
+pesquisa.addEventListener('keydown', (e) => {
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        // TODO: limpar dados ao buscar
 
-        retorno.forEach(colaborador => {
-            const li = document.createElement('li');
+        let nome = document.getElementById('nome-colaborador').value;
+        listarColaboradores(nome);
+    }
+});
 
-            li.textContent = `
-                Nome: ${colaborador.nome}; 
-                CPF: ${colaborador.cpf};
-                Email: ${colaborador.email};
-                Cargo: ${colaborador.cargo?.descricao};
-                Data Nascimento: ${colaborador.dataNascimento};
-                Data Admissão: ${colaborador.dataNascimento};
-                `;
 
-            lista.append(li);
-        });
-
-        section.append(lista);
+function listarColaboradores(nome = '') {
+    fetch(`http://localhost:8080/colaboradores?nome=${nome}`, {
+        method: 'GET'
     })
-    .catch(error => {
-        console.error(error);
-    });
+        .then(response => response.json())
+        .then(retorno => {
+            const tabela = document.getElementById('tabela');
+
+            retorno.forEach(colaborador => {
+                tabela.innerHTML += montarLinhaTabela(colaborador.nome);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+function montarLinhaTabela(nome) {
+    return `<tr>
+        <td class="icone-usuario">
+            <i class="material-icons">person</i>
+        </td>
+        <td class="nome">${nome}</td>
+        <td class="acoes">
+            <i class="material-icons excluir">delete</i>
+            <i class="material-icons editar">edit</i>
+            <i class="material-icons visualizar">visibility</i>
+        </td>
+    </tr>`;
+}
+
+listarColaboradores();
