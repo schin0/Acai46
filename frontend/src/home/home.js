32da52bf3dd2931850201062d83b55d57b8a7ad0
Gabriel Schincariol <gschin11@gmail.com>
2023-05-20 15:37:56 -0300

    Inserir função que limpe tabela ao filtrar

--- frontend/src/home/home.js
+++ frontend/src/home/home.js
@@ -3,7 +3,6 @@ const pesquisa = document.getElementById('nome-colaborador');
 pesquisa.addEventListener('keydown', (e) => {
     if (e.keyCode === 13) {
         e.preventDefault();
-        // TODO: limpar dados ao buscar
 
         let nome = document.getElementById('nome-colaborador').value;
         listarColaboradores(nome);
@@ -12,15 +11,17 @@ pesquisa.addEventListener('keydown', (e) => {
 
 
 function listarColaboradores(nome = '') {
+    const tabela = document.getElementById('tabela');
+    tabela.innerHTML = '';
+
     fetch(`http://localhost:8080/colaboradores?nome=${nome}`, {
         method: 'GET'
     })
         .then(response => response.json())
         .then(retorno => {
-            const tabela = document.getElementById('tabela');
 
             retorno.forEach(colaborador => {
-                tabela.innerHTML += montarLinhaTabela(colaborador.nome);
+                tabela.innerHTML += montarLinhaTabela(colaborador.nome, colaborador.id);
             });
         })
         .catch(error => {
@@ -28,18 +29,26 @@ function listarColaboradores(nome = '') {
         });
 }
 
-function montarLinhaTabela(nome) {
+function montarLinhaTabela(nome, id) {
     return `<tr>
-        <td class="icone-usuario">
-            <i class="material-icons">person</i>
-        </td>
-        <td class="nome">${nome}</td>
-        <td class="acoes">
-            <i class="material-icons excluir">delete</i>
-            <i class="material-icons editar">edit</i>
-            <i class="material-icons visualizar">visibility</i>
-        </td>
-    </tr>`;
+                <td class="icone-usuario">
+                    <i class="material-icons">person</i>
+                </td>
+                <td class="nome">${nome}</td>
+                <td class="acoes">
+                    <i class="material-icons excluir">delete</i>
+                    <i class="material-icons editar">edit</i>
+                    <i class="material-icons visualizar" onclick="visualizarColaborador(${id})">visibility</i>
+                </td>
+            </tr>`;
+}
+
+function visualizarColaborador(id) {
+    const url = new URLSearchParams();
+    url.set('id', id);
+
+    const paginaColaborador = `../colaborador/colaborador.html?${url.toString()}`;
+    window.location.href = paginaColaborador;
 }
 
 listarColaboradores();
